1911858fbf4ac6ab860147a8737ae5f6f9125e05
jairbave <jair.barriosv@uniagustiniana.edu.co>
2023-02-28 19:42:51 -0500

    se agregaron 6 archivoa, 3 clases y 3codigos fuentes

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal{
+  public static void main(String arg[]){
+  Sedan chevrolet = new Sedan();
+  chevrolet.encender();
+  System.out.println(chevrolet.acelerar(30));
+  System.out.println(chevrolet.getVelocidad());
+  }
+}
